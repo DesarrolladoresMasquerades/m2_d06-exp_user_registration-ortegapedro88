c485f0ebd7d8856e2c4e79a15916f5b2e2431d1c
Pedro Ortega Escande <ortegapedro88@gmail.com>
2022-02-14 17:21:15 +0100

    done

--- config/index.js
+++ config/index.js
@@ -1,15 +1,34 @@
 const express = require("express");
 const logger = require("morgan");
 const cookieParser = require("cookie-parser");
+const session = require("express-session");
 const favicon = require("serve-favicon");
+const MongoStore = require("connect-mongo");
 const path = require("path");
+require("dotenv/config");
 
 module.exports = (app) => {
   app.use(logger("dev"));
   app.use(express.json());
-  app.use(express.urlencoded({ ex
+  app.use(express.urlencoded({ extend: true }));
+  app.use(cookieParser());
+  app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    saveUninitialized: false,
+    resave: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI,
+      ttl: 24 * 60 * 60
+    }),
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000, //one day old
+    }
+  }))
+
   app.set("views", path.join(__dirname, "..", "views"));
   app.set("view engine", "hbs");
   app.use(express.static(path.join(__dirname, "..", "public")));
-  app.use(favicon(path.join(__dirname, "..", "public", "images", "favicon.ico")));
+  app.use(
+    favicon(path.join(__dirname, "..", "public", "images", "favicon.ico"))
+  );
 };
